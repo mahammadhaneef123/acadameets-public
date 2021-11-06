7d8df0450d63ff446be585795585030d18e34176
RimsKadapaGovt <RimsKadapaGovt@users.noreply.github.com>
2021-11-06 17:51:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const http = require('http');
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Welcome to ACADAMEETS');
-})
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
